5d6ab5c58ddaef31e34767adb566d97deb22bcc2
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2023-07-27 14:40:21 -0500

    fix: :mag: Update opengraph image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import "@/styles/globals.css";
 import { Metadata } from "next";
 
-
-
 import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
@@ -11,6 +9,7 @@ import { SiteHeader } from "@/components/site-header"
 import { ThemeProvider } from "@/components/theme-provider"
 
 import AnalyticWrapper from "./analytics"
+
 
 export const metadata: Metadata = {
   title: {
@@ -23,10 +22,6 @@ export const metadata: Metadata = {
     title: "Channel4",
     description: "The React Framework for the Web",
     creator: "@StateChannel_4",
-    images: "social-image.png",
-  },
-  openGraph: {
-    images: ["social-image.png"],
   },
   themeColor: [
     { media: "(prefers-color-scheme: light)", color: "white" },
