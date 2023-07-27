8799b0f40af4bc6c3f4e68cc8ad014fff06a150d
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2023-07-27 15:05:24 -0500

    fix: :speech_balloon: Fix SEO description for twitter

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,19 @@
 import "@/styles/globals.css";
 import { Metadata } from "next";
 
-import { siteConfig } from "@/config/site"
-import { fontSans } from "@/lib/fonts"
-import { cn } from "@/lib/utils"
-import Feedback from "@/components/feedback"
-import { SiteHeader } from "@/components/site-header"
-import { ThemeProvider } from "@/components/theme-provider"
 
-import AnalyticWrapper from "./analytics"
+
+import { siteConfig } from "@/config/site";
+import { fontSans } from "@/lib/fonts";
+import { cn } from "@/lib/utils";
+import Feedback from "@/components/feedback";
+import { SiteHeader } from "@/components/site-header";
+import { ThemeProvider } from "@/components/theme-provider";
+
+
+
+import AnalyticWrapper from "./analytics";
+
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://channel4.wtf"),
@@ -23,7 +28,7 @@ export const metadata: Metadata = {
   twitter: {
     card: "summary_large_image",
     title: "Channel4",
-    description: "The React Framework for the Web",
+    description: siteConfig.description,
     creator: "@StateChannel_4",
   },
   themeColor: [
