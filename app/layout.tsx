e25142c05fc430b67677060f7eeb08a240c4f669
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2023-07-26 21:03:38 -0500

    feat: :mag: Update SEO metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,11 @@
 import "@/styles/globals.css";
-import { Metadata } from "next";
+import { Metadata } from "next"
 
-
-
-import { siteConfig } from "@/config/site";
-import { fontSans } from "@/lib/fonts";
-import { cn } from "@/lib/utils";
-import Feedback from "@/components/feedback";
-import { SiteHeader } from "@/components/site-header";
+import { siteConfig } from "@/config/site"
+import { fontSans } from "@/lib/fonts"
+import { cn } from "@/lib/utils"
+import Feedback from "@/components/feedback"
+import { SiteHeader } from "@/components/site-header"
 import { ThemeProvider } from "@/components/theme-provider"
 
 import AnalyticWrapper from "./analytics"
@@ -15,9 +13,19 @@ import AnalyticWrapper from "./analytics"
 export const metadata: Metadata = {
   title: {
     default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
+    template: `%s | ${siteConfig.name}`,
   },
   description: siteConfig.description,
+  twitter: {
+    card: "summary_large_image",
+    title: "Channel4",
+    description: "The React Framework for the Web",
+    creator: "@StateChannel_4",
+    images: ["/social-image.png"],
+  },
+  openGraph: {
+    images: ["/social-image.png"],
+  },
   themeColor: [
     { media: "(prefers-color-scheme: light)", color: "white" },
     { media: "(prefers-color-scheme: dark)", color: "black" },
