9544d7320b75d18a8162935b88b55bf7d3fa16b8
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2023-08-02 16:12:01 -0500

    refactor: :chart_with_upwards_trend: Change analytics to Matomo

--- app/analytics.tsx
+++ app/analytics.tsx
@@ -1,14 +1,27 @@
-"use client"
+"use client";
 
-import { useSwetrix } from "@swetrix/nextjs"
+import { useEffect } from "react"
+
+// import { useSwetrix } from "@swetrix/nextjs"
 
 export default function AnalyticWrapper({
   children,
 }: {
   children: React.ReactNode
 }) {
-  useSwetrix(`${process.env.NEXT_PUBLIC_SWETRIX}`, {
-    debug: true,
-  })
+  useEffect(() => {
+    // @ts-ignore
+    var _mtm = (window._mtm = window._mtm || [])
+    _mtm.push({ "mtm.startTime": new Date().getTime(), event: "mtm.Start" })
+    ;(function () {
+      var d = document,
+        g = d.createElement("script"),
+        s = d.getElementsByTagName("script")[0]
+      g.async = true
+      g.src = process.env.NEXT_PUBLIC_MATOMO_URL || ""
+
+      if (s.parentNode) s.parentNode.insertBefore(g, s)
+    })()
+  }, [])
   return <div className="flex-1">{children}</div>
 }
