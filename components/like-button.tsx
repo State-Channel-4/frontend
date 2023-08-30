bb4c78dccc97ee959cab93516ec7d4eef623efd1
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2023-08-30 18:25:05 -0500

    refactor: :sparkles: Refactor `LikeButton` into its own component

---
+++ components/like-button.tsx
@@ -0,0 +1,66 @@
+import { ThumbsUpIcon } from "lucide-react"
+import { Button } from "./ui/button"
+import { cn } from "@/lib/utils"
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover"
+import Link from "next/link"
+import { C4Content } from "@/types"
+
+interface LikeButtonProps {
+  signedIn: boolean,
+  likeOrUnlike: (arg0: string) => void
+  site: C4Content
+  userLikes: string[]
+}
+
+const LikeButton = ({ signedIn, likeOrUnlike, site, userLikes }: LikeButtonProps) => {
+  const isLiked = userLikes.includes(site._id)
+
+  const LikeButtonContent = () => (
+    <Button
+      variant="default"
+      size="sm"
+      className="group inline-flex items-center gap-2 self-start rounded-full px-4 py-2 text-sm transition-all duration-300"
+      onClick={signedIn ? (e) => likeOrUnlike(site._id) : undefined}
+    >
+      <ThumbsUpIcon
+        size={16}
+        className={cn(
+          "transition-all duration-300 group-hover:-rotate-12 group-hover:scale-110 group-hover:text-yellow-300",
+          isLiked && "text-yellow-300"
+        )}
+        aria-label="Like"
+      />
+      <p className="text-primary">
+        {isLiked ? "Liked" : "Like"}
+      </p>
+      <p>•</p>
+      <p>{site.likes}</p>
+    </Button>
+  );
+
+
+  if (signedIn) {
+    return <LikeButtonContent />;
+  } else {
+    return (
+      <Popover>
+        <PopoverTrigger>
+          <LikeButtonContent />
+        </PopoverTrigger>
+        <PopoverContent
+          className="flex px-2 py-1 w-full bg-c4-gradient-blue rounded-xl"
+          side="top"
+        >
+          <Link href="/sign-in" passHref>
+            <Button variant={"link"} size={"sm"}>
+              Sign in to like
+            </Button>
+          </Link>
+        </PopoverContent>
+      </Popover>
+    );
+  }
+}
+
+export default LikeButton
+
