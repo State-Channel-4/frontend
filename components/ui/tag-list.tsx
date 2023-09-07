611c7419cd2f9c01c11d72c9532fc4829acde79d
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2023-09-07 13:43:52 -0500

    refactor: :recycle: Refactor `TagList`
    `TagButton` is now its own memoized component and `handleDiscover` will check if any tags were selected before updating session

--- components/ui/tag-list.tsx
+++ components/ui/tag-list.tsx
@@ -1,14 +1,21 @@
 "use client"
 
-import { useState } from "react"
-import { useRouter } from "next/navigation"
 import { Tag, TagMap } from "@/types"
 import { CheckCircle2, PlusCircle } from "lucide-react"
+import { useRouter } from "next/navigation"
+import { memo, useCallback, useMemo, useState } from "react"
 
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
 
 import { buttonVariants } from "./button"
+
+interface TagButtonProps {
+  tag: Tag;
+  selectedTags: TagMap;
+  onSelect: (selectedTag: Tag) => void;
+  selectable: boolean;
+}
 
 interface TagListProps {
   tags: TagMap
@@ -16,32 +23,60 @@ interface TagListProps {
   selectable?: boolean
 }
 
+const TagButton = memo(({ tag, selectedTags, onSelect, selectable }: TagButtonProps) => {
+  const isSelected = useMemo(() => selectedTags.has(tag._id), [
+    selectedTags,
+    tag,
+  ])
+
+  return (
+    <button
+      className={cn(
+        "border-primary/30 text-primary flex cursor-default items-center justify-center gap-2 rounded-full border-2 px-3 py-1 text-sm font-medium transition",
+        selectable &&
+        "hover:bg-primary/30 focus:bg-primary/40 focus-visible:ring-offset-muted hover:cursor-pointer focus:outline-none focus-visible:ring-2 focus-visible:ring-yellow-400 focus-visible:ring-offset-2",
+        isSelected ? "border-yellow-400" : ""
+      )}
+      tabIndex={0}
+      onClick={() => onSelect(tag)}
+    >
+      {selectable && (
+        <span>
+          {isSelected ? (
+            <CheckCircle2 size={16} className="text-yellow-400" />
+          ) : (
+            <PlusCircle size={16} />
+          )}
+        </span>
+      )}
+      {tag.name}
+    </button>
+  )
+})
+
 const TagList = ({ tags, title, selectable = false }: TagListProps) => {
   const router = useRouter()
   const [selectedTags, setSelectedTags] = useState<TagMap>(new Map())
 
-  const handleSelectedTags = (selectedTag: Tag) => {
-    const _selectedTags = selectedTags
-    if (_selectedTags.has(selectedTag._id)) {
-      _selectedTags.delete(selectedTag._id)
-      setSelectedTags(_selectedTags)
-    } else {
-      _selectedTags.set(selectedTag._id, selectedTag)
-      setSelectedTags(new Map(_selectedTags))
-    }
-  }
+  const handleSelectedTags = useCallback((selectedTag: Tag) => {
+    setSelectedTags((currentTags) => {
+      const newTags = new Map(currentTags)
+      if (newTags.has(selectedTag._id)) {
+        newTags.delete(selectedTag._id)
+      } else {
+        newTags.set(selectedTag._id, selectedTag)
+      }
+      return newTags
+    })
+  }, [])
 
   const handleDiscover = () => {
-    sessionStorage.setItem(
-      "c4.tags",
-      JSON.stringify(
-        Array.from(
-          selectedTags.size === 0
-            ? new Map(tags).entries()
-            : selectedTags.entries()
-        )
+    if (selectedTags.size > 0) {
+      sessionStorage.setItem(
+        "c4.tags",
+        JSON.stringify(Array.from(selectedTags.entries()))
       )
-    )
+    }
     router.push(siteConfig.links.discover)
   }
 
@@ -59,32 +94,13 @@ const TagList = ({ tags, title, selectable = false }: TagListProps) => {
           {title && <h2 className="text-lg font-semibold">{title}</h2>}
           <div className="flex flex-wrap gap-2">
             {Array.from(tags).map(([key, tag]) => (
-              <button
-                className={cn(
-                  "border-primary/30 text-primary flex cursor-default items-center justify-center gap-2 rounded-full border-2 px-3 py-1 text-sm font-medium transition",
-                  selectable &&
-                    "hover:bg-primary/30 focus:bg-primary/40 focus-visible:ring-offset-muted hover:cursor-pointer focus:outline-none focus-visible:ring-2 focus-visible:ring-yellow-400 focus-visible:ring-offset-2",
-                  selectable &&
-                    selectedTags.size > 0 &&
-                    selectedTags.has(tag._id)
-                    ? "border-yellow-400"
-                    : ""
-                )}
+              <TagButton
                 key={key}
-                tabIndex={0}
-                onClick={() => handleSelectedTags(tag)}
-              >
-                {selectable && (
-                  <span>
-                    {selectedTags.size > 0 && selectedTags.has(tag._id) ? (
-                      <CheckCircle2 size={16} className="text-yellow-400" />
-                    ) : (
-                      <PlusCircle size={16} />
-                    )}
-                  </span>
-                )}
-                {tag.name}
-              </button>
+                tag={tag}
+                selectedTags={selectedTags}
+                onSelect={handleSelectedTags}
+                selectable={selectable}
+              />
             ))}
           </div>
         </div>
