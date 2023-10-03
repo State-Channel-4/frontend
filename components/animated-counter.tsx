9a136fd712647040c2463598334dfc0f8b648aa8
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2023-10-03 16:21:19 -0500

    refactor: :dizzy: Timer animation will now pause on each number

--- components/animated-counter.tsx
+++ components/animated-counter.tsx
@@ -1,18 +1,17 @@
 'use client'
-import { motion } from 'framer-motion'
-
+import { motion } from 'framer-motion';
 
 const CountdownTimer = ({ from }: { from: number }) => {
   return (
     <section className='h-[180px] w-[270px] overflow-hidden rounded-full lg:h-[300px] lg:w-[400px] xl:h-[360px] xl:w-[440px]'>
       <motion.div
         initial={{ y: 0 }}
-        animate={{ y: "-91.42%" }}
+        animate={{ y: ["-0.2%", "-9%", "-18%", "-27%", "-37%", "-45%", "-55%", "-64%", "-73%", "-82%", "-91%"] }}
         transition={{
           duration: from,
           ease: [0.42, 0, 0.58, 1],
           times: [0, 0.2, 0.5, 0.8, 1],
-          delay: 0.1
+          delay: 0.6
         }}
         className='relative flex flex-col items-center overflow-hidden'
       >
