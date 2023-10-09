22822f5314d06d3b18028bf688c1df195eced99b
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2023-10-09 17:53:06 -0500

    fix: :label: Add missing type for `createProxyUrls` fn

--- app/utils.ts
+++ app/utils.ts
@@ -1,3 +1,5 @@
+import { C4Content } from "@/types";
+
 export const createProxyUrls = (sites: C4Content[]) => {
     return sites.map(site => {
         return { ...site, proxyUrl: `https://corsproxy.io/?${encodeURIComponent(site.url)}` };
