67d71e065d424e646a6e024dd09708546b0121a3
Ian-Bright <ianbrighton1@gmail.com>
2023-12-04 21:44:12 -0700

    feat: initial dashboard design set up

---
+++ app/(stateless-toolbar)/dashboard/page.tsx
@@ -0,0 +1,27 @@
+const Dashboard = () => {
+  return (
+    <div className="flex justify-center items-center flex-col h-full">
+      <div className="px-4">
+        <div className="text-shark-300 font-bold text-2xl/none">Login with</div>
+        <div className="mt-6 text-shark-100 text-[32px]/none">
+          user’s email if they use social
+        </div>
+        <div className="text-shark-200 mt-4">Member since July 19, 2023</div>
+        <div className="text-shark-300 font-bold text-2xl/none mt-12">
+          Websites submitted
+        </div>
+        <div className="text-shark-50 font-extrabold mt-6 text-[56px]/none">
+          109
+        </div>
+        <div className="text-shark-300 font-bold text-2xl/none mt-10">
+          Likes submitted
+        </div>
+        <div className="text-shark-50 font-extrabold mt-6 text-[56px]/none">
+          4
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
