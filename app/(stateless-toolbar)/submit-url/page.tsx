963efb7d9abcba931a2ce0c887bce7bf832f665b
Ian-Bright <ianbrighton1@gmail.com>
2023-12-06 09:10:45 -0700

    feat: Clean up RequireAuth component, add loading state, and allow signin via web3auth through it

--- app/(stateless-toolbar)/submit-url/page.tsx
+++ app/(stateless-toolbar)/submit-url/page.tsx
@@ -157,155 +157,158 @@ const SubmitUrl = () => {
   }, [])
 
   return (
-    <div
-      className="flex h-full items-start justify-center bg-shark-900 bg-center bg-no-repeat p-2 md:items-center"
-      style={{
-        backgroundImage: `url(${Background.src})`,
-        backgroundSize: "90%",
-      }}
-    >
-      <div className="h-full w-full max-w-[1130px] rounded-[32px] bg-c4-gradient-separator p-px md:h-[501px]">
-        <div className="h-full overflow-y-auto rounded-[32px] bg-shark-950 p-4 pb-16 md:overflow-visible md:p-10">
-          <div className="flex h-full flex-col items-start justify-between gap-10 md:flex-row">
-            <div className="w-full flex-1">
-              <div className="flex items-center gap-4 text-4xl">
-                <div className="md:whitespace-nowrap">Add a website</div>
-                <Image alt="Browser" className="h-8 w-8" src={BrowserIcon} />
-              </div>
-              <div className="mt-10 text-xl text-shark-50">Website URL</div>
-              <input
-                className={`mt-4 w-full rounded-lg border-[1.5px] bg-shark-950 ${
-                  duplicateUrlError ? "border-red-500" : "border-shark-800"
-                } p-3 text-lg placeholder:text-shark-400`}
-                placeholder="Paste URL here"
-                onChange={(e) => handleUrlInput(e.target.value)}
-                value={url ?? ""}
-              />
-              {duplicateUrlError && (
-                <div className="mt-1 text-xs text-red-500">
-                  * Url already exists
+    <RequireAuth>
+      <div
+        className="flex h-full items-start justify-center bg-shark-900 bg-center bg-no-repeat p-2 md:items-center"
+        style={{
+          backgroundImage: `url(${Background.src})`,
+          backgroundSize: "90%",
+        }}
+      >
+        <div className="h-full w-full max-w-[1130px] rounded-[32px] bg-c4-gradient-separator p-px md:h-[501px]">
+          <div className="h-full overflow-y-auto rounded-[32px] bg-shark-950 p-4 pb-16 md:overflow-visible md:p-10">
+            <div className="flex h-full flex-col items-start justify-between gap-10 md:flex-row">
+              <div className="w-full flex-1">
+                <div className="flex items-center gap-4 text-4xl">
+                  <div className="md:whitespace-nowrap">Add a website</div>
+                  <Image alt="Browser" className="h-8 w-8" src={BrowserIcon} />
                 </div>
-              )}
-              <div className="mt-6 flex items-center gap-2">
-                <div className="text-xl text-shark-50">Short description</div>
-                <div className="text-sm text-shark-300">(Optional)</div>
-              </div>
-              <input
-                className="mt-4 w-full rounded-lg border-[1.5px] border-shark-800 bg-shark-950 p-3 text-lg placeholder:text-shark-400"
-                onChange={(e) => setDescription(e.target.value)}
-                placeholder="This site is about..."
-                value={description}
-              />
-              <div className="mt-6 flex flex-wrap items-center justify-between gap-2">
-                <div className="text-xl text-shark-50">Choose tags</div>
-                {showTagInput ? (
-                  <div className="flex items-center gap-2">
-                    <div className="flex grow items-center">
-                      {!creatingTag && (
+                <div className="mt-10 text-xl text-shark-50">Website URL</div>
+                <input
+                  className={`mt-4 w-full rounded-lg border-[1.5px] bg-shark-950 ${
+                    duplicateUrlError ? "border-red-500" : "border-shark-800"
+                  } p-3 text-lg placeholder:text-shark-400`}
+                  placeholder="Paste URL here"
+                  onChange={(e) => handleUrlInput(e.target.value)}
+                  value={url ?? ""}
+                />
+                {duplicateUrlError && (
+                  <div className="mt-1 text-xs text-red-500">
+                    * Url already exists
+                  </div>
+                )}
+                <div className="mt-6 flex items-center gap-2">
+                  <div className="text-xl text-shark-50">Short description</div>
+                  <div className="text-sm text-shark-300">(Optional)</div>
+                </div>
+                <input
+                  className="mt-4 w-full rounded-lg border-[1.5px] border-shark-800 bg-shark-950 p-3 text-lg placeholder:text-shark-400"
+                  onChange={(e) => setDescription(e.target.value)}
+                  placeholder="This site is about..."
+                  value={description}
+                />
+                <div className="mt-6 flex flex-wrap items-center justify-between gap-2">
+                  <div className="text-xl text-shark-50">Choose tags</div>
+                  {showTagInput ? (
+                    <div className="flex items-center gap-2">
+                      <div className="flex grow items-center">
+                        {!creatingTag && (
+                          <Button
+                            className="h-fit p-2"
+                            onClick={() => hideNewTag()}
+                            variant="ghost"
+                          >
+                            <Ban size={16} />
+                          </Button>
+                        )}
                         <Button
-                          className="h-fit p-2"
-                          onClick={() => hideNewTag()}
+                          className="h-fit grow p-2"
+                          loading={creatingTag}
+                          loaderIconSize={14}
+                          loadingText="Creating"
+                          onClick={() => !creatingTag && createTag()}
                           variant="ghost"
                         >
-                          <Ban size={16} />
+                          Create
+                          {!creatingTag && <Plus size={16} />}
                         </Button>
-                      )}
-                      <Button
-                        className="h-fit grow p-2"
-                        loading={creatingTag}
-                        loaderIconSize={14}
-                        loadingText="Creating"
-                        onClick={() => !creatingTag && createTag()}
-                        variant="ghost"
-                      >
-                        Create
-                        {!creatingTag && <Plus size={16} />}
-                      </Button>
+                      </div>
                     </div>
-                  </div>
+                  ) : (
+                    <Button
+                      className="h-fit p-2"
+                      onClick={() => setShowTagInput(true)}
+                      variant="ghost"
+                    >
+                      <div>Create tag</div>
+                      <Plus size={14} />
+                    </Button>
+                  )}
+                </div>
+                {showTagInput ? (
+                  <input
+                    className={`mt-4 w-full rounded-lg  border-[1.5px] p-3 ${
+                      duplicateTagError ? "border-red-500" : "border-shark-800"
+                    } bg-shark-950 px-2 placeholder:text-shark-400`}
+                    onChange={(e) => handleTagInput(e.target.value)}
+                    placeholder="Tag name..."
+                    value={newTag}
+                  />
                 ) : (
-                  <Button
-                    className="h-fit p-2"
-                    onClick={() => setShowTagInput(true)}
-                    variant="ghost"
-                  >
-                    <div>Create tag</div>
-                    <Plus size={14} />
-                  </Button>
+                  <Select
+                    onSelect={addSelected}
+                    onRemove={removeSelected}
+                    options={optionNames}
+                    selected={selectedNames}
+                  />
+                )}
+                {duplicateTagError && (
+                  <div className="mt-1 text-xs text-red-500">
+                    * Tag already exists
+                  </div>
                 )}
               </div>
-              {showTagInput ? (
-                <input
-                  className={`mt-4 w-full rounded-lg  border-[1.5px] p-3 ${
-                    duplicateTagError ? "border-red-500" : "border-shark-800"
-                  } bg-shark-950 px-2 placeholder:text-shark-400`}
-                  onChange={(e) => handleTagInput(e.target.value)}
-                  placeholder="Tag name..."
-                  value={newTag}
-                />
-              ) : (
-                <Select
-                  onSelect={addSelected}
-                  onRemove={removeSelected}
-                  options={optionNames}
-                  selected={selectedNames}
-                />
-              )}
-              {duplicateTagError && (
-                <div className="mt-1 text-xs text-red-500">
-                  * Tag already exists
-                </div>
-              )}
-            </div>
-            <div className="flex h-full w-full flex-1 flex-col">
-              <div className="text-xl text-shark-50">Preview</div>
-              <SubmitSiteFrame url={url} />
-              <div className="mt-4 flex items-center justify-between">
-                <div className="flex items-center gap-2">
-                  <input
-                    className="h-[18px] w-[18px] shrink-0 cursor-pointer rounded border border-shark-500 bg-shark-700"
-                    checked={previewPasses}
-                    onChange={() => setPreviewPasses(!previewPasses)}
-                    type="checkbox"
-                  />
-                  <div className="text-sm text-shark-200">
-                    Check this box if the website preview looks good
-                  </div>
-                </div>
-                <Popover>
-                  <PopoverTrigger className="relative flex cursor-pointer items-center gap-2 text-sm disabled:cursor-not-allowed">
-                    <Info
-                      className="cursor-pointer stroke-shark-300"
-                      size={16}
+              <div className="flex h-full w-full flex-1 flex-col">
+                <div className="text-xl text-shark-50">Preview</div>
+                <SubmitSiteFrame url={url} />
+                <div className="mt-4 flex items-center justify-between">
+                  <div className="flex items-center gap-2">
+                    <input
+                      className="h-[18px] w-[18px] shrink-0 cursor-pointer rounded border border-shark-500 bg-shark-700"
+                      checked={previewPasses}
+                      onChange={() => setPreviewPasses(!previewPasses)}
+                      type="checkbox"
                     />
-                  </PopoverTrigger>
-                  <PopoverContent
-                    align="end"
-                    alignOffset={-15}
-                    className="w-64 rounded-lg border border-shark-800 bg-shark-950 p-4 text-left text-sm text-shark-50"
-                    side="top"
-                    sideOffset={15}
-                  >
-                    Some websites don&apos;t allow other applications to render
-                    their website in an iframe. Make sure you the preview
-                    renders correctly so others can enjoy this channel.
-                  </PopoverContent>
-                </Popover>
-              </div>
-              <div className="my-8 md:mb-0">
-                <Slider
-                  disabled={!previewPasses || !selectedTags.length || !url}
-                  error={errorSending}
-                  onSubmit={onClickShareItHandler}
-                  sending={isSending}
-                  sent={sent}
-                />
+                    <div className="text-sm text-shark-200">
+                      Check this box if the website preview looks good
+                    </div>
+                  </div>
+                  <Popover>
+                    <PopoverTrigger className="relative flex cursor-pointer items-center gap-2 text-sm disabled:cursor-not-allowed">
+                      <Info
+                        className="cursor-pointer stroke-shark-300"
+                        size={16}
+                      />
+                    </PopoverTrigger>
+                    <PopoverContent
+                      align="end"
+                      alignOffset={-15}
+                      className="w-64 rounded-lg border border-shark-800 bg-shark-950 p-4 text-left text-sm text-shark-50"
+                      side="top"
+                      sideOffset={15}
+                    >
+                      Some websites don&apos;t allow other applications to
+                      render their website in an iframe. Make sure you the
+                      preview renders correctly so others can enjoy this
+                      channel.
+                    </PopoverContent>
+                  </Popover>
+                </div>
+                <div className="my-8 md:mb-0">
+                  <Slider
+                    disabled={!previewPasses || !selectedTags.length || !url}
+                    error={errorSending}
+                    onSubmit={onClickShareItHandler}
+                    sending={isSending}
+                    sent={sent}
+                  />
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </RequireAuth>
   )
 }
 
--- components/helper/RequireAuth.tsx
+++ components/helper/RequireAuth.tsx
@@ -1,9 +1,8 @@
 "use client"
 
-import Link from "next/link"
+import { useAuth } from "@/contexts/AuthContext"
 import { useJwtStore } from "@/store/jwt"
-
-import { siteConfig } from "@/config/site"
+import { Loader2 } from "lucide-react"
 
 /**
  * check signin status and navigate to
@@ -16,15 +15,26 @@ interface Props {
 }
 
 const RequireAuth = ({ children }: Props) => {
+  const { initializingW3A, signIn } = useAuth()
   const { userId, token } = useJwtStore()
-  if (!token || !userId) {
+  if (initializingW3A) {
     return (
-      <div className="mx-7 my-10 flex flex-col items-center justify-center text-center lg:container">
+      <div className="flex h-full flex-col items-center justify-center px-4">
+        <h2>Checking Web3Auth Connection...</h2>
+        <Loader2 className="mt-4 animate-spin stroke-c4-green" size={36} />
+      </div>
+    )
+  } else if (!token || !userId) {
+    return (
+      <div className="flex h-full flex-col items-center justify-center px-4 text-center">
         <h2 className="my-5">
           Sign in{" "}
-          <Link href={siteConfig.links.signIn} className="text-green-500">
+          <button
+            className="cursor-pointer text-c4-green"
+            onClick={() => signIn()}
+          >
             here
-          </Link>{" "}
+          </button>{" "}
           to view this section
         </h2>
       </div>
