cb992534b91cf5fcb7b538e2626fcad6f48fa16d
Ian-Bright <ianbrighton1@gmail.com>
2023-12-19 03:40:14 -0700

    fix: Change height of main content wrapper to not cause scroll on mobile

--- components/main-content-wrapper.tsx
+++ components/main-content-wrapper.tsx
@@ -6,8 +6,7 @@ export default function MainContentWrapper({
   children: ReactNode
 }): JSX.Element {
   return (
-    // TODO: Remove hacky height fix once background image is replaced with countdown component
-    <div className="h-[calc(100vh-136px)] flex-1 p-4 md:px-8 md:pb-0 md:pt-6">
+    <div className="h-[calc(100svh-144px)] flex-1 p-4 md:px-8 md:pb-0 md:pt-6">
       <div className="h-full w-full overflow-hidden rounded-2xl border border-shark-600">
         {children}
       </div>
